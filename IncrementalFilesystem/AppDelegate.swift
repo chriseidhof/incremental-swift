c8b84d1ffab0ace75c902f9e6767e11e864f6b4a
Chris Eidhof <chris@eidhof.nl>
2017-08-20 18:57:25 +0200

    Reorg and event handling

--- IncrementalFilesystem/AppDelegate.swift
+++ IncrementalFilesystem/AppDelegate.swift
@@ -11,12 +11,24 @@ import Incremental
 import CoreServices
 
 final class DirectoryObserver {
-    let stream: FSEventStreamRef
-    let runloop: CFRunLoop
+    var stream: FSEventStreamRef! = nil
+    let runloop: CFRunLoop!
     
     init?(paths: [String], observer: () -> ()) {
-        guard let stream = FSEventStreamCreate(nil, callback, self, paths.map { $0 as CFString } as CFArray, UInt64(kFSEventStreamEventIdSinceNow), 1, UInt32(kFSEventStreamCreateFlagNone)) else { return nil }
         runloop = CFRunLoopGetCurrent()
+        var streamContext = FSEventStreamContext(version: 0, info: Unmanaged.passRetained(self).toOpaque(), retain: nil, release: nil, copyDescription: nil)
+        let callback: FSEventStreamCallback = {
+            (ref: ConstFSEventStreamRef, clientInfo: UnsafeMutableRawPointer?, numEvents: Int, eventPaths: UnsafeMutableRawPointer, eventFlags: UnsafePointer<FSEventStreamEventFlags>, eventIds: UnsafePointer<FSEventStreamEventId>) in
+            let paths = Unmanaged<NSArray>.fromOpaque(eventPaths).takeUnretainedValue() as! [String]
+            dump(eventFlags.pointee)
+            print(numEvents)
+            //  as! [String]
+            print("callback! \(paths)")
+        }
+        let flags = FSEventStreamCreateFlags(kFSEventStreamCreateFlagUseCFTypes
+            | kFSEventStreamCreateFlagIgnoreSelf
+            | kFSEventStreamCreateFlagFileEvents)
+        guard let stream = FSEventStreamCreate(nil, callback, &streamContext, paths.map { $0 as CFString } as CFArray, UInt64(kFSEventStreamEventIdSinceNow), 1, flags) else { return nil }
         FSEventStreamScheduleWithRunLoop(stream, runloop, CFRunLoopMode.defaultMode.rawValue)
         self.stream = stream
         FSEventStreamStart(stream)
@@ -38,8 +50,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        observer = DirectoryObserver(paths: ["/Users/chris/Desktop/Incremental/sample"]) { event in
-            print(event)
+        observer = DirectoryObserver(paths: ["/Users/chris/Desktop/Incremental/sample"]) {
+            print("event")
         }
         
     }
